c5c87f53dc30c41f653e6f03711e1644987773ca
santosh9949 <rageersantosh1010@gmail.com>
2024-10-27 01:29:39 -0500

    updated

---
+++ confis.js
@@ -0,0 +1,9 @@
+module.exports = {
+  platform: 'local',
+  repositories: ["/tmp/renovate"],
+  logLevel: "debug",
+  endpoint: "https://api.local-renovate.local",
+  localDir: "/tmp/renovate"
+};
+
+
